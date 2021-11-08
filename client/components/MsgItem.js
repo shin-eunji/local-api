9c98c91bd05bdad18f7b7d41c4073028314b0637
shin-eunji <dmswlckato@gmail.com>
2021-11-08 23:24:18 +0900

    editing note

---
+++ client/components/MsgItem.js
@@ -0,0 +1,32 @@
+import MsgInput from "./MsgInput";
+
+const MsgItem = ({id, userId, timestamp, text, onUpdate, isEditing, startEdit, onDelete}) => (
+  <li className="messages__item">
+    <h3>
+      {userId}{' '}
+      <sub>
+        {new Date(timestamp).toLocaleDateString('ko-KR', {
+          year: "numeric",
+          month: "numeric",
+          day: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: true,
+        })}
+      </sub>
+    </h3>
+
+    {isEditing ? (
+      <>
+        <MsgInput mutate={onUpdate} id={id} text={text} />
+      </>) : (text)
+    }
+
+    <div className="messages__buttons">
+      <button onCLick={startEdit}>수정</button>
+      <button onCLick={onDelete}>삭제</button>
+    </div>
+  </li>
+)
+
+export default MsgItem;
